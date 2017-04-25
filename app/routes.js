9955942b2e2090fc2438dbc9ae13074f611d85b3
radiance90 <henry.j.yu@hotmail.com>
2017-04-25 17:01:39 +1000

    update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,14 @@
 var Movie = require('./models/movie');
 var router = require('express').Router();
-module.exports = function(app) {
-	
+
+module.exports = function(app, passport) {
+	router.get('/', function(req, res) {
+		res.json({message: "Welcome!"});
+	});
+
+	app.use('/api', router);
+
+	app.get('*', function(req. res) {
+		res.sendfile('./public/views.index.html');
+	});
 }
